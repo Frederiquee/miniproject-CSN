985b3f125d27c046b5c2a50b1a60822517010602
Frederiquee <31880309+Frederiquee@users.noreply.github.com>
2017-11-10 15:33:01 +0100

    Update Client.py

--- Client.py
+++ Client.py
@@ -35,24 +35,24 @@ def knopjes():
             Rood = GPIO.output(13, False)
             Groen = GPIO.output(7, True)
 
-        if (GPIO.input(12)):
+        if (GPIO.input(12)): #Als knop rechts is ingedrukt, gaat geel aan 
             print('Sensor is afgegaan')
-            Geel = GPIO.output(11, True)
+            Geel = GPIO.output(11, True) #Geel gaat aan
             Rood = GPIO.output(13, False)
             Groen = GPIO.output(7, False)
             time.sleep(4)
 
-            if (GPIO.input(16)):
+            if (GPIO.input(16)): #Als knop links is ingedrukt, gaat groen aan
                 print('Code ingevoerd om alarm uit te schakelen')
                 Geel = GPIO.output(11, False)
-                Groen = GPIO.output(7, True)
+                Groen = GPIO.output(7, True)#Groen gaat aan
             else:
                 Geel = GPIO.output(11, False)
-                Rood = GPIO.output(13, True)
+                Rood = GPIO.output(13, True) #Rood gaat aan als linker knop niet wordt ingedrukt 
                 Groen = GPIO.output(7, False)
-                sockConnectie.send(b'Alarm is afgegaan')
+                sockConnectie.send(b'Alarm is afgegaan') #'Alarm is afgegaan' wordt naar server gestuurd 
                 print('Alarm is afgegaan')
-        if (Pingen != 0):
+        if (Pingen != 0): # Ping met server 192.168.42.9
             print("Het alarmsysteem is niet meer connected met de server")
             GPIO.output(13, True) # Rood gaat aan, alarm is ingeschakeld client is niet meer verbonden met server
             GPIO.output(7, False)
@@ -61,16 +61,16 @@ def knopjes():
 def aanEnUitAlarmsysteem():
     while True:
         info = sockConnectie.recv(1024)
-        if info == b'aan':
-            GPIO.output(7, True)
+        if info == b'aan': #Als cliënt 'aan' ontvangt van de server, dan gaat groene licht aan 
+            GPIO.output(7, True) # Groen gaat aan 
             GPIO.output(13, False)
 
-        if info == b'uit':
+        if info == b'uit': #Als cliënt 'uit' ontvangt van de server, gaan alle lichten uit
             GPIO.output(7, False)
             GPIO.output(11, False)
             GPIO.output(13, False)
 
-        if info == b'shutdown':
+        if info == b'shutdown': # Als cliënt 'shutdown' van de server ontvangt wordt de connectie verbroken
             print('Client uitgeschakeld vanuit de server')
             sockConnectie.close()
 
